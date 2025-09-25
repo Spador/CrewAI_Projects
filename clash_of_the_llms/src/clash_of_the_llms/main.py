7e42e12fd94426b35b0d97becdae8c57fb9c84ef
Spador <sp3466@rit.edu>
2025-09-25 08:28:46 -0400

    setting up crew.py and main.py

--- clash_of_the_llms/src/clash_of_the_llms/main.py
+++ clash_of_the_llms/src/clash_of_the_llms/main.py
@@ -8,18 +8,12 @@ from clash_of_the_llms.crew import ClashOfTheLlms
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
 
-# This main file is intended to be a way for you to run your
-# crew locally, so refrain from adding unnecessary logic into this file.
-# Replace with inputs you want to test with, it will automatically
-# interpolate any tasks and agents information
-
 def run():
     """
     Run the crew.
     """
     inputs = {
-        'topic': 'AI LLMs',
-        'current_year': str(datetime.now().year)
+        'motion': 'There needs to be strict laws to regulate LLMs',
     }
     
     try:
